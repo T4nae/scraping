72a145b10e626d2c074f73e4a9f716c056e876de
T4nae <tan.b63000@gmail.com>
2022-07-09 16:24:00 +0530

    abilty to login from saved cookies

--- youtube.py
+++ youtube.py
@@ -1,6 +1,7 @@
 from utils.crawlers import crawler
 from utils.data import data
 from getpass import getpass
+from os import path
 import argparse
 
 def extract_data(source, args):
@@ -57,25 +58,31 @@ def subsciptions(args):
     """
     url = 'https://www.youtube.com/feed/subscriptions'
     crawl = crawler(url)
-    crawl.Selenium(undetected=True, headless=False)  # can't run in headless mode since UC headlessmode is still WIP
-    crawl.driver.implicitly_wait(5)
-    if crawl.locate_presence('LINK', 'SIGN IN'):
-        crawl.find('LINK', 'SIGN IN').click()
-        if args.pwd == '':
-            pwd = getpass('Enter Password:')
-        else:
-            pwd = args.pwd    
+    if path.exists('youtubecookie.pkl') and args.cookie == True:
+      crawl.Selenium(undetected=True, headless=False)  # can't run in headless mode since UC headlessmode is still WIP
+      crawl.add_cookies('youtubecookie.pkl')
+      crawl.get_url(url)
+      crawl.driver.implicitly_wait(5)
+      crawl.locate_presence('XPATH', '//*[@id="endpoint"]/tp-yt-paper-item/yt-formatted-string')
+    else:
+      crawl.Selenium(undetected=True, headless=False)  # can't run in headless mode since UC headlessmode is still WIP
+      crawl.driver.implicitly_wait(5)
+      crawl.find('LINK', 'SIGN IN').click()
+      if args.pwd == '':
+          pwd = getpass('Enter Password:')
+      else:
+          pwd = args.pwd    
  
-        loginBox = crawl.find('XPATH', '//*[@id ="identifierId"]')
-        loginBox.send_keys(args.id)
-        nextButton = crawl.find('XPATH', '//*[@id ="identifierNext"]')
-        nextButton.click()
-        passWordBox = crawl.find('XPATH', '//*[@id ="password"]/div[1]/div / div[1]/input')
-        passWordBox.send_keys(pwd)
-        nextButton = crawl.find('XPATH', '//*[@id ="passwordNext"]')
-        nextButton.click()
-        crawl.locate_presence('XPATH', '//*[@id="endpoint"]/tp-yt-paper-item/yt-formatted-string')
-              
+      loginBox = crawl.find('XPATH', '//*[@id ="identifierId"]')
+      loginBox.send_keys(args.id)
+      nextButton = crawl.find('XPATH', '//*[@id ="identifierNext"]')
+      nextButton.click()
+      passWordBox = crawl.find('XPATH', '//*[@id ="password"]/div[1]/div / div[1]/input')
+      passWordBox.send_keys(pwd)
+      nextButton = crawl.find('XPATH', '//*[@id ="passwordNext"]')
+      nextButton.click()
+      crawl.locate_presence('XPATH', '//*[@id="endpoint"]/tp-yt-paper-item/yt-formatted-string')
+      crawl.get_cookies('youtubecookie.pkl')        
 
     source = crawl.get_source()
     crawl.close()
@@ -120,7 +127,9 @@ if __name__ == "__main__":
     parser.add_argument('--links', type=bool, default=True,
       help='to enable links of the videos in output and enabled by default. ')
     parser.add_argument('--v', type=bool , default=True,
-      help='to enable show data in commandline in output and enabled by default. ')  
+      help='to enable show data in commandline in output and enabled by default. ')
+    parser.add_argument('--cookie', type=bool, default=True,
+      help='to enable use of cookie to login to last used account')  
 
     args = parser.parse_args()  
 
