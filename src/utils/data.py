c943898886566cf5a1fc9ab009396c69f14d60da
T4nae <tan.b63000@gmail.com>
2022-07-20 15:53:46 +0530

    added better system to handle data

---
+++ src/utils/data.py
@@ -0,0 +1,86 @@
+import csv
+import pandas as pd
+from os import path
+from bs4 import BeautifulSoup
+from lxml import etree
+
+
+class data:
+    def __init__(self, source= ''):
+        self.dataframe = None
+        self.path = '../data/data'
+        self.source = source
+        
+    def create_csv(self, headers):
+        """
+        creates a unique csv file with headers
+        """
+        
+        while path.exists(self.path + '.csv'):
+            if self.path.endswith(')') and self.path[-2:-1].isnumeric() and self.path[-3:-2] == '(':
+                self.path = self.path[:-2] + str(int(self.path[-2:-1]) + 1) + ')'
+            else:
+                self.path = self.path + '(1)'
+            
+        with open(self.path + '.csv', 'w') as file:
+            writer = csv.writer(file)
+            writer.writerow(headers) 
+               
+    def write_csv(self, row):
+        """
+        write data into row at a time in already created csv file
+        """
+        with open(self.path, 'a', encoding='UTF8') as file:
+            writter = csv.writer(file)
+            writter.writerow(row)
+        
+    def concatenate(self, lists):
+        """
+        concatenate list of lists together and make csv file
+        """
+
+        data = []
+        for nelmnts in range(len(lists[0])): # no of elements in list
+            for nlsts in range(len(lists)):  # no of lists 
+                data.append(lists[nlsts][nelmnts])   
+                #print(lists[nlsts][nelmnts])
+            self.write_csv(data)
+            data = []
+        
+    def view_data(self):
+        """
+        prints dataframe from csv file in a more readable manner
+        """
+        self.dataframe = pd.read_csv(self.path)
+        return self.dataframe
+
+    def find_by_id(self, div, ids, type='text'):
+        """
+        find data in source by div and id
+        """
+        data = []
+        
+        soup = BeautifulSoup(self.source, 'lxml')
+        if type == 'text':
+            for selector in soup.find_all(div, id= ids):
+                data.append(selector.text)
+
+        elif type == 'links':    
+            for selector in soup.find_all('a', id='video-title', href=True):
+                data.append(selector['href'])
+
+        return data
+
+
+    def find_by_xpath(self, xpath):
+        """
+        find data in source by their xpath
+        """
+        data = []
+        soup = BeautifulSoup(self.source, 'lxml')
+
+        dom = etree.HTML(str(soup))
+        for element in dom.xpath(xpath):
+            data.append(element.text)
+
+        return data
